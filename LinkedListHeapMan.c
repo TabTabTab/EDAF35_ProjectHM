530ea47fb803c9203d86491b63793f25836073ed
Johan Nyholm <hansjohannyholm@gmail.com>
2015-05-09 14:16:00 +0200

    simple implementation of malloc

---
+++ LinkedListHeapMan.c
@@ -0,0 +1,87 @@
+#include <stddef.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+
+
+
+typedef struct node_t node_t;
+
+struct node_t{
+	size_t size;
+	node_t* next;
+	bool is_free;
+};
+
+void* malloc(size_t size); 
+void free(void* ptr); 
+
+
+node_t* get_avail(size_t size);
+node_t* create_block(size_t size,node_t** last);
+static const size_t block_node_size=512;
+
+static const void* failed_sbrk=(void*) -1;
+static node_t* free_head=NULL;
+
+
+void* malloc(size_t size){
+	printf("calling custom malloc\n");
+	node_t* node=get_avail(size);
+
+	if(node==NULL){
+		return NULL;
+	}
+	node->is_free=false;	
+	//node+1 in order to evade returning the node itself, node+1 will return a ptr to the actual data block
+	return node+1;
+}
+
+/*
+Returns null if failure
+*/
+node_t* get_avail(size_t size)
+{
+	//first try to find a usable block
+	node_t* temp=free_head;
+
+	//we need to keep track of last so that we can add to the last element later on
+	node_t** last=&free_head;
+	while(temp!=NULL){
+		if(temp->is_free && (temp->size) >= size){
+			return temp;
+		}
+		node_t** last=&(temp->next);
+		temp=temp->next;
+	}
+
+	node_t* new_block=create_block(size,last);
+	return new_block;
+}
+
+node_t* create_block(size_t size,node_t** last)
+{
+	node_t* new_node=sbrk(0);
+
+	void* request = sbrk(block_node_size+size);
+	if (request==failed_sbrk){
+		return NULL;
+	}
+	memset(request,0,block_node_size+size);
+	*last=new_node;
+	return new_node;
+}
+
+
+int main()
+{
+	printf("\n==================\nwe are running..\n==================\n\n");
+	void* mem=malloc(10);
+	void* mem2=malloc(10);
+	printf("mem: %d\n",mem);
+	printf("mem2: %d\n",mem2);
+
+	
+}
